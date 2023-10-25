af7e6179b088dafc3189481d73fc06492c0843ae
shizucc <syifanaalfi@gmail.com>
2023-10-25 15:35:37 +0700

    expansion done

--- lib/app/modules/workspaces/controller/animation_controller_workspace.dart
+++ lib/app/modules/workspaces/controller/animation_controller_workspace.dart
@@ -5,13 +5,18 @@ class WorkspaceAnimationController extends GetxController {
   final _isOpen = <bool>[].obs;
 
   // Init _isOpen for each Panel
-  bool isOpen() {
-    final int length = _isOpen.length;
-    return _isOpen[length];
+  void initIsOpen(int length) {
+    for (int i = 0; i < length; i++) {
+      _isOpen.add(true);
+    }
+  }
+
+  bool isOpen(int index) {
+    return _isOpen[index];
   }
 
   // Switch
   void switchPanel(int i, bool isOpen) {
-    _isOpen[i] = !isOpen;
+    _isOpen[i] = isOpen;
   }
 }
--- lib/app/modules/workspaces/views/workspaces_view.dart
+++ lib/app/modules/workspaces/views/workspaces_view.dart
@@ -38,88 +38,7 @@ class WorkspacePage extends StatelessWidget {
               return const Text("Tidak ada data.");
             } else {
               final workspaces = snapshot.data!;
-              return ListView(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                children: workspaces.map((workspace) {
-                  // Mendapatkan nama workspace (ini kode ngakalin)
-                  return FutureBuilder<String>(
-                    future: c.getWorkspaceName(workspace),
-                    builder: (context, nameSnapshot) {
-                      if (nameSnapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return const CircularProgressIndicator();
-                      } else if (nameSnapshot.hasError) {
-                        return Text("Error: ${nameSnapshot.error}");
-                      } else {
-                        final workspaceName = nameSnapshot.data;
-                        final projects = c.fetchProjects(workspace);
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                const Icon(
-                                  CupertinoIcons.chevron_up,
-                                  size: 25,
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    workspaceName.toString(),
-                                    style: const TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ),
-                                const Icon(CupertinoIcons.ellipsis)
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            FutureBuilder<List<QueryDocumentSnapshot<Project>>>(
-                              future: projects,
-                              builder: (context, projectSnapshot) {
-                                if (projectSnapshot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return const CircularProgressIndicator();
-                                } else if (projectSnapshot.hasError) {
-                                  return Text(
-                                      "Error: ${projectSnapshot.error}");
-                                } else if (!projectSnapshot.hasData ||
-                                    projectSnapshot.data!.isEmpty) {
-                                  return const Text("Invalid Name");
-                                } else {
-                                  final projectList = projectSnapshot.data!;
-                                  return Column(
-                                    children: projectList.map((project) {
-                                      return ProjectTile(
-                                          title: project.data().name,
-                                          image: project
-                                              .data()
-                                              .personalize['image'],
-                                          onTap: () {
-                                            Get.toNamed(Routes.PROJECT,
-                                                arguments: {
-                                                  "workspace": workspace,
-                                                  "project": project
-                                                });
-                                          });
-                                    }).toList(),
-                                  );
-                                }
-                              },
-                            ),
-                            const SizedBox(height: 10)
-                          ],
-                        );
-                      }
-                    },
-                  );
-                }).toList(),
-              );
+              return WorkspaceExpansion(workspaces: workspaces, c: c);
             }
           },
         ),
@@ -128,16 +47,107 @@ class WorkspacePage extends StatelessWidget {
   }
 }
 
-class ExpansionWorkspaces extends StatelessWidget {
-  const ExpansionWorkspaces({super.key});
+class WorkspaceExpansion extends StatelessWidget {
+  final List<DocumentReference<Workspace>> workspaces;
+  final WorkspaceController c;
+  const WorkspaceExpansion(
+      {super.key, required this.workspaces, required this.c});
+
   @override
   Widget build(BuildContext context) {
     final a = Get.put(WorkspaceAnimationController());
-    return ExpansionPanelList(
-      children: [
-        // ExpansionPanel(
-        //     headerBuilder: headerBuilder, body: body, isExpanded: a.isOpen())
-      ],
+    a.initIsOpen(workspaces.length);
+    return ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      children: workspaces.asMap().entries.map((entry) {
+        // Mendapatkan nama workspace (ini kode ngakalin)
+        final int index = entry.key;
+        final workspace = entry.value;
+        return FutureBuilder(
+            future: c.getWorkspaceName(workspace),
+            builder: (context, nameSnapshot) {
+              if (nameSnapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (nameSnapshot.hasError) {
+                return Text("Error: ${nameSnapshot.error}");
+              } else {
+                final workspaceName = nameSnapshot.data;
+                final projects = c.fetchProjects(workspace);
+                return Obx(() => Column(
+                      children: [
+                        Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  a.switchPanel(index, !a.isOpen(index));
+                                },
+                                icon: a.isOpen(index)
+                                    ? const Icon(
+                                        CupertinoIcons.chevron_up,
+                                        size: 25,
+                                      )
+                                    : const Icon(
+                                        CupertinoIcons.chevron_down,
+                                        size: 25,
+                                      )),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: Text(
+                                workspaceName.toString(),
+                                style: const TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                            const Icon(CupertinoIcons.ellipsis)
+                          ],
+                        ),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeOut,
+                          child: a.isOpen(index) == true
+                              ? FutureBuilder<
+                                  List<QueryDocumentSnapshot<Project>>>(
+                                  future: projects,
+                                  builder: (context, projectSnapshot) {
+                                    if (projectSnapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return const CircularProgressIndicator();
+                                    } else if (projectSnapshot.hasError) {
+                                      return Text(
+                                          "Error: ${projectSnapshot.error}");
+                                    } else if (!projectSnapshot.hasData ||
+                                        projectSnapshot.data!.isEmpty) {
+                                      return const Text("Invalid Name");
+                                    } else {
+                                      final projectList = projectSnapshot.data!;
+                                      return Column(
+                                        children: projectList.map((project) {
+                                          return ProjectTile(
+                                              title: project.data().name,
+                                              image: project
+                                                  .data()
+                                                  .personalize['image'],
+                                              onTap: () {
+                                                Get.toNamed(Routes.PROJECT,
+                                                    arguments: {
+                                                      "workspace": workspace,
+                                                      "project": project
+                                                    });
+                                              });
+                                        }).toList(),
+                                      );
+                                    }
+                                  },
+                                )
+                              : null,
+                        )
+                      ],
+                    ));
+              }
+            });
+      }).toList(),
     );
   }
 }
